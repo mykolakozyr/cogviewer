6afb93d8df43d60db52a6bbce54901fec9c1989b
mykolakozyr <mykola.kozyr@gmail.com>
2021-07-25 15:25:55 +0200

    Adding GTM to app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ code = """
       gtag('config', '"""+tag+"""');
     </script>
 """
-a = pathlib.Path(st.__path__[0]) / 'static/index.html'
+a = pathlib.Path(st.__path__[0]) / 'static/app.py'
 #a=os.path.dirname(st.__file__)+'/static/index.html'
 with open(a, 'r') as f:
     data=f.read()
